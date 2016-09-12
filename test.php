20367609f6bf3dba7fe8a11cd6decae578c0cadb
Patrick Turner <TurnerP@cwu.edu>
2016-09-12 14:07:36 -0700

    Delete files for cleanup

--- test.php
+++
@@ -1,9 +0,0 @@
-$results2 = gather_results($db->query('SELECT `team_code`,`teams_full_name`,`city` FROM `teams` WHERE `teams_id` < 36 ORDER BY `team_code`'));
-	$team_options = '';
-	$team_json = '{';
-	foreach($results2 as $team){
-		$team_options .= '<option value="'.$team['team_code'].'" title="'.$team['team_code'].'" >'.$team['team_code'].' - '.$team['teams_full_name'].'</option>';
-		$team_json .= $team['team_code'].':"'.$team['teams_full_name'].'",';
-	}
-	$team_json = rtrim($team_json, ",");
-	$team_json .= '}';
