892d7056458813f32330ad033e61a23c55d15ef8
Patrick Turner <TurnerP@cwu.edu>
2019-01-19 03:10:38 -0800

    Clear Dev From Master

--- Website/adminDW/PHP/Includes/DSH_Email.php
+++
@@ -1,8 +0,0 @@
-<h2>Email List</h2>
-<div>
-	<form action="PHP/sendEmail.php" method="post" id="emailComposeForm">
-		<input type="text" name="mail_subject" value="NFL Arrest Update: " />
-		<textarea name="mail_body"></textarea>
-		<input type="submit" name="form_action" value="Send Email" />
-	</form>
-</div>
