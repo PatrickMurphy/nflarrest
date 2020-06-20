5eb1c9585496f5c379fe7d56a46d6d455a6b6154
Patrick Murphy <patrick@patrickmurphyphoto.com>
2020-06-19 22:40:24 -0700

    Fix detail page step 1

--- public_html/js/DetailPage.js
+++ public_html/js/DetailPage.js
@@ -5,6 +5,7 @@ class DetailPage {
 		this.arrestsUrl = arrestsUrl; // api/team/arrests.php?id=
 		this.chartOptions = chartOptions; // [{url:'',field:'',targetElement:'',title:''}]
 		this.arrest_view_mode = 0; // 0 = table, 1 = card (Mobile Default)
+		this.data_controller;
 
 		this.callbackReturns = 0;
 		this.charts = [];
@@ -21,23 +22,26 @@ class DetailPage {
 		});
 
 		dateRangeController.init(function (newDateRange) {
-			self.dateRangeNFL = newDateRange;
+			DataController.init(function (newDataController) {
+				self.data_controller = newDataController;
+				self.dateRangeNFL = newDateRange;
 
-			var page_dimension = self.pageTitle.toLowerCase();
-			var filters_options = {
-				presets: {},
-				date_range_object: self.dateRangeNFL
-			};
-			filters_options['presets'][page_dimension] = {};
-			filters_options['presets'][page_dimension][page_dimension] = self.pageID;
-			self.FilterControl = new FiltersControl(filters_options);
-			$(self.FilterControl.options.dialog_element).on('FilterDialogChanged', function () {
-				console.log('render view filter');
-				// todo not working, using the hash change global for now
+				var page_dimension = self.pageTitle.toLowerCase();
+				var filters_options = {
+					presets: {},
+					date_range_object: self.dateRangeNFL
+				};
+				filters_options['presets'][page_dimension] = {};
+				filters_options['presets'][page_dimension][page_dimension] = self.pageID;
+				self.FilterControl = new FiltersControl(filters_options);
+				$(self.FilterControl.options.dialog_element).on('FilterDialogChanged', function () {
+					console.log('render view filter');
+					// todo not working, using the hash change global for now
+					self.renderView();
+				});
+
 				self.renderView();
-			});
-
-			self.renderView();
+			})
 		});
 	}
 
@@ -66,24 +70,77 @@ class DetailPage {
 
 	renderArrests() {
 		var self = this;
-		$.getJSON(this.arrestsUrl + this.pageID + '?' + this.FilterControl.getQueryString(), function (data) {
-			var row,
-				items = [];
-			$('body > div.container > section > div > h4').html(data.length + ' Incidents:');
-			if (self.arrest_view_mode == 0) {
-				items.push(self.renderArrestRowHeader());
-			}
-			for (var rowID in data) {
-				row = data[rowID];
-				if (self.arrest_view_mode == 0) {
-					items.push(self.renderArrestRow(row));
-				} else if (self.arrest_view_mode == 1) {
-					items.push(self.renderArrestCard(row));
-				}
-			}
+		self.data_controller.getArrests(function(row){
+				/*//this.arrestsUrl + this.pageID + '?' + this.FilterControl.getQueryString();
+				var filterValueSet = self.FilterControl.getFilterValues();
+				// copy presets to value set
+				filterValueSet = Object.assign(filterValueSet, self.FilterControl.options.presets);
 
-			$('#arrest_table').html(items.join(""));
-			self.checkLoadingFinished();
+				// build query string
+				var querystring_return = [];
+				for (var filterKey in filterValueSet) {
+					for (var filterSubKey in filterValueSet[filterKey]) {
+						if (Array.isArray(filterValueSet[filterKey][filterSubKey])) {
+							var returnVal = false;
+							for (var filterSubSubKey in filterValueSet[filterKey][filterSubKey]) {
+								if(row[filterSubKey] == filterValueSet[filterKey][filterSubKey][filterSubSubKey]){
+									returnVal = true;
+								}
+								//querystring_return.push(filterSubKey + '=' + filterValueSet[filterKey][filterSubKey][filterSubSubKey]);
+							}
+							if(returnVal === false){
+								return false;
+							}
+						} else {
+							if(row[filterSubKey] != filterValueSet[filterKey][filterSubKey]){
+								return false;
+							}
+							//querystring_return.push(filterSubKey + '=' + filterValueSet[filterKey][filterSubKey]);
+						}
+					}
+				}
+
+				return true;
+				*/
+				if(self.pageTitle == 'Team'){
+					if(row['Team'] != self.pageID){
+						return false;
+					}
+				}else if(self.pageTitle == 'Position'){
+					if(row['Position'] != self.pageID){
+						return false;
+					}
+				}else if(self.pageTitle == 'Player'){
+					if(row['Name'] != self.pageID){
+						return false;
+					}
+				}else if(self.pageTitle == 'Crime'){
+					if(row['Category'] != self.pageID){
+						return false;
+					}
+				}
+
+				return true;
+			}, function (data) {
+				var row,
+					items = [];
+
+				$('body > div.container > section > div > h4').html(data.length + ' Incidents:');
+				if (self.arrest_view_mode == 0) {
+					items.push(self.renderArrestRowHeader());
+				}
+
+				for (var rowID in data) {
+					row = data[rowID];
+					if (self.arrest_view_mode == 0) {
+						items.push(self.renderArrestRow(row));
+					} else if (self.arrest_view_mode == 1) {
+						items.push(self.renderArrestCard(row));
+					}
+				}
+
+				$('#arrest_table').html(items.join(""));
+				self.checkLoadingFinished();
 		});
 	}
 
