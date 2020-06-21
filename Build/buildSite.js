0234a82599aeed74d703d55bca235ed8b12d45e9
Patrick Murphy <patrick@patrickmurphyphoto.com>
2020-06-20 18:55:51 -0700

    add ghpage build script

---
+++ Build/buildSite.js
@@ -0,0 +1,10 @@
+var ghpages = require('gh-pages');
+
+// run DB update procs (cache arrest state, cache last arrests, cache arrest table)
+// build json from db
+// minify homepage
+// build detailPage html
+// minify detailPage JS 
+ghpages.publish('Website', function(err) {
+	console.log(err);
+});
