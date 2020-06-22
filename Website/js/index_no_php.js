18b873b34eb777a2f4dfb8c6df5ee85d6bfefdf2
Patrick Murphy <patrick@patrickmurphyphoto.com>
2020-06-21 22:18:00 -0700

    changes to index for detail page

--- Website/js/index_no_php.js
+++ Website/js/index_no_php.js
@@ -6,7 +6,7 @@ var last_start_pos = 0,
 	listsReturned = false,
 	ytdChart = false,
 	mainChartStyleID = 0
-	detail_page_active = true;
+	detail_page_active = false;
 
 //removed for es6 var nflLoadingBar;
 
@@ -125,7 +125,7 @@ function renderActivePlayerArrests() {
 
 function loadTeamLinks(data) {
 	$.each(data, function (key, val) {
-		$('#bottomTeamLinks').append('<a href="team/' + val.Team + '/"><span style="display:inline-block;width:20px;height:20px;vertical-align: text-bottom;background:url(\'images/NFLTeamLogos.png\') 0px -' + (val.Team_logo_id * 20) + 'px;background-size:100%;"></span> ' + val.Team_preffered_name + '</a> ');
+		$('#bottomTeamLinks').append('<a href="' + getPageLink("team", val.Team) + '/"><span style="display:inline-block;width:20px;height:20px;vertical-align: text-bottom;background:url(\'images/NFLTeamLogos.png\') 0px -' + (val.Team_logo_id * 20) + 'px;background-size:100%;"></span> ' + val.Team_preffered_name + '</a> ');
 	});
 }
 
@@ -256,6 +256,10 @@ function setupChart() {
 	});
 }
 
+function getPageLink(page,value){
+	return page + "Cache.html#" + value;
+}
+
 function load_top_list(data, page, prefix, list, values, replace) {
 	replace = replace || false;
 	if (replace) {
@@ -264,7 +268,7 @@ function load_top_list(data, page, prefix, list, values, replace) {
 	var items = [];
 	if (data.length > 0) {
 		$.each(data, function (key, val) {
-			var link = "<a href=\"" + page + "/" + val[values[0]] + "/\">";
+			var link = "<a href=\"" + getPageLink(page, val[values[0]]) + "/\">";
 			var link_end = '</a>';
 			if (page == '' || !detail_page_active) {
 				link = '';
