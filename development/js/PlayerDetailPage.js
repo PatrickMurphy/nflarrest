89a0ea87b342ea229f765c2f210654bf532ea3e0
Murphy <patrick.murphy@lcb.wa.gov>
2021-11-24 13:58:55 -0800

    Bug Fix: Player Page donut chart undefined

--- development/js/PlayerDetailPage.js
+++ development/js/PlayerDetailPage.js
@@ -10,7 +10,7 @@ class PlayerDetailPage extends DetailPage {
             }, {
             type: 'donut',
             url: 'api/v1/player/topCrimes/',
-            field: 'category',
+            field: 'Crime_category',
             targetElement: '#crimeschart',
             title: 'Crimes'
             }], 'api/v1/player/arrests/');
