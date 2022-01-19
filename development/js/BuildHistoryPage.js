d94e1c716fc59912d05f15bd34a4c2d268cd007a
PatrickMurphy <patrick@patrickmurphyphoto.com>
2022-01-19 03:58:48 -0800

    BUILD development 2.11.34: Commit || 1/19/2022 03:58

--- development/js/BuildHistoryPage.js
+++ development/js/BuildHistoryPage.js
@@ -35,7 +35,7 @@ class BuildHistoryPage extends WebPage {
             var devIndent = '----';
             var versionLink = `<li><a href="#v${value['build_release_version'].replace('.','-')}" ${styleBold}>V${value['build_release_version']}</a></li>`;
             if(value['build_environment_name'] === 'Development'){
-                var versionLink = `<li><a href="#v${value['build_release_version'].replace('.','-')}">V${devIndent+value['build_release_version']}</a></li>`;
+                var versionLink = `<li><a href="#v${value['build_release_version'].replace('.','-')}">${devIndent+"V"+value['build_release_version']}</a></li>`;
             }
             //if(lastItemWasProd){
               //  versionLink += value['build_release_version'] + ' <ol><li';
--- development/js/data/lastUpdate_data.js
+++ development/js/data/lastUpdate_data.js
@@ -1,1 +1,1 @@
-var lastUpdate = "1/19/2022, 3:56:21 AM"; var lastVersion = "2.11.33";
+var lastUpdate = "1/19/2022, 3:58:45 AM"; var lastVersion = "2.11.34";
