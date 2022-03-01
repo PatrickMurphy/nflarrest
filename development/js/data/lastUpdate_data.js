a1890e63cf9b949622f912e2fcc5b44de5c656f6
PatrickMurphy <patrick@patrickmurphyphoto.com>
2022-03-01 04:41:58 -0800

    BUILD development Local 2.28.36: Local Build || 3/1/2022 04:41

--- development/js/data/lastUpdate_data.js
+++ development/js/data/lastUpdate_data.js
@@ -1,1 +1,1 @@
-var lastUpdate = "3/1/2022, 4:39:56 AM"; var lastVersion = "2.28.35";
+var lastUpdate = "3/1/2022, 4:41:55 AM"; var lastVersion = "2.28.36";
--- development/js/modules/DataTable.Module.js
+++ development/js/modules/DataTable.Module.js
@@ -95,14 +95,12 @@ class DataTable extends Module {
         this.defaultFunctions.displayDataCallbackFn = ((data) => {
             self.setupContainerElements(data);
             self.parent.checkLoadingFinished();
-            console.log('run default displayDataCallbackFn');
 		});
     }
     
     
     // setupContainerTitle function resets the html of the table container and adds a title
     setupContainerTitle(data){
-        console.log('setup container title');
         var tableContainer = this.getOption('targetElementTableContainer') || 'arrest_details_container';
         var incidentSelector = this.getOption('targetElementTitleIncidentCount') || 'arrest_details_incident_count'; //'body > div.container > section > div > h4'
         var h4Prefix = this.getOption('TitlePrefix') || '';
@@ -110,9 +108,6 @@ class DataTable extends Module {
         // add empty h4 element as only element in container
         $('#'+tableContainer).html('<h4 id="'+incidentSelector+'" style="text-align:left;"># Incidents:</h4>');
         $('#'+incidentSelector).html(h4Prefix + data.length + ' Incidents:');
-        
-        console.log('setup container title end','#'+tableContainer,'#'+incidentSelector,h4Prefix);
-        console.log($('#'+tableContainer).contents());
     }
     
     // setupContainerElements Function adds the table element or card container to the table container after the h4 heading. 
@@ -122,19 +117,15 @@ class DataTable extends Module {
         var incidentSelector = this.getOption('targetElementTitleIncidentCount') || 'arrest_details_incident_count'; //'body > div.container > section > div > h4'
         // if add html elements for each display mode
         if (this.view_mobile) {
-            console.log('setup container elements mobile', this.view_mobile, '#'+incidentSelector);
             // add arrest cards container
             $('#'+incidentSelector).after('<div id="'+this.getOption('targetElementMobile')+'"></div>');
             // add pagination control
             $('#'+this.getOption('targetElementMobile')).after('<div id="pagination-control"></div>');
         } else {
-            console.log('setup container elements desktop ', this.view_mobile, '#'+incidentSelector,this.getOption('targetElement'));
             // add table
             $('#'+incidentSelector).after('<table id="'+this.getOption('targetElement')+'"></table>');
-            console.log($('#'+incidentSelector).html());
             // add pagination control
             $('#'+this.getOption('targetElement')).after('<div id="pagination-control"></div>');
-            console.log($('#'+incidentSelector).html());
         }
     }
     
